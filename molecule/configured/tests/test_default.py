f09055268839358a65de46ba6817bb78fb435296
Gabriel Suarez <gabriel.suarez@solarwinds.com>
2020-01-24 19:32:42 +0000

    updated tests for testing a package on each distribution

--- molecule/configured/tests/test_default.py
+++ molecule/configured/tests/test_default.py
@@ -7,8 +7,28 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_pkg_installed(host):
-    pkg_1 = host.package("iotop")
-    pkg_2 = host.package("unzip")
+    distro = host.system_info.distribution
+    version = host.system_info.release
 
-    assert pkg_1.is_installed
-    assert pkg_2.is_installed
+    if distro == "centos" and version.startswith("6"):
+        pkg = host.package("iptables")
+        assert pkg.is_installed
+
+    elif distro == "centos" and version.startswith("7"):
+        pkg = host.package("tmux")
+        assert pkg.is_installed
+
+    elif distro == "amazon" and version.startswith("1"):
+        pkg = host.package("telnet")
+        assert pkg.is_installed
+
+    elif distro == "amazon" and version.startswith("2"):
+        pkg = host.package("zsh")
+        assert pkg.is_installed
+
+    elif distro == "ubuntu" and version.startswith("18"):
+        pkg = host.package("apt-file")
+        assert pkg.is_installed
+
+    pkg = host.package("wget")
+    assert pkg.is_installed
